c194987de60c0f73f918bee63516dbcc6542b623
Rodrigo Nairnei <ronairnei@gmail.com>
2021-10-20 21:21:00 -0300

    feat: add very basic layout concept #1

--- app/src/main/java/dev/nairnei/stonewallet/view/pages/home/HomeActivity.kt
+++ app/src/main/java/dev/nairnei/stonewallet/view/pages/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package dev.nairnei.stonewallet
+package dev.nairnei.stonewallet.view.pages.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.nairnei.stonewallet.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
